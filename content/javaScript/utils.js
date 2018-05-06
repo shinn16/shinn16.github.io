57d52e88f8c6057baf856bda8eadad268bad1343
Patrick Shinn <shinn16@marshall.edu>
2018-05-05 23:32:17 -0400

    changing to js

--- content/javaScript/utils.js
+++ content/javaScript/utils.js
@@ -4,3 +4,14 @@ function setYear(){
   year = new Date().getFullYear();
   document.write(year);
 }
+
+
+// easy function for tab interfaces
+function openTab(tabName) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
