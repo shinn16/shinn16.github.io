4a33662ee80a30b1aeafe2a02028c54e6e1d5db4
Patrick Shinn <shinn16@marshall.edu>
2018-05-05 23:54:56 -0400

    moving some css to js

--- content/javaScript/utils.js
+++ content/javaScript/utils.js
@@ -7,11 +7,16 @@ function setYear(){
 
 
 // easy function for tab interfaces
-function openTab(tabName) {
+function openTab(tabName, label) {
     var i;
     var x = document.getElementsByClassName("tab");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
+    var x = document.getElementsByClassName("tabs");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.color = "white";
+    }
     document.getElementById(tabName).style.display = "block";
+    label.style.color = "#20a4f3";
 }
